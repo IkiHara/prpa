6a2633708159751f3238ebe754e92b1132a96a8d
IkiHara <riecher.mistt@gmail.com>
2019-12-01 02:20:27 +0100

    hw08

--- hw08/main.c
+++ hw08/main.c
@@ -4,115 +4,42 @@
 
 int main(int argc, char *argv[])
 {
-    int a, b;
+    int c = 0, s = 0, l = 0, se = 0, le = 0;
     
-
-    if(argc == 2)
+    if(argc > 6)
+        return 100;
+    
+    for(int i = 1; i < argc; ++i)
     {
-        printf("pocet argumentu = 1\n");
-        if(strcmp(argv[1], "-c") == 0)
-            printf("argument 1 je %s\n", argv[1]);
-        else
-        {
+        if(strcmp(argv[i], "-c") == 0)
+            c = 1;
+        else if(strcmp(argv[i], "-s") == 0)
+            se = 1;
+        else if(strcmp(argv[i], "-l") == 0)
+            le = 1;
+        else if(strcmp(argv[i], "1") && strcmp(argv[i-1], "-s") == 0)
+            s = atoi(argv[i]);
+        else if(strcmp(argv[i], "2") && strcmp(argv[i-1], "-s") == 0)
+            s = atoi(argv[i]);
+        else if(strcmp(argv[i-1], "-l") == 0)
+            l = atoi(argv[i]);
+        else{
             printf("zadany parametr neni spravny\n");
             return 100;
         }
-        
+        printf("%s\n", argv[i]);
+        printf("c = %i\n", c);
+        printf("s = %i\n", s);
+        printf("l = %i\n", l);
     }
-    else if(argc == 3)
-    {
-        printf("pocet argumentu je 2\n");
-        printf("argument 1 je %s\n", argv[1]);
-        if(strcmp(argv[1], "-c") == 0)
-            printf("argument 1 je %s\n", argv[1]);
-        else if(strcmp(argv[1], "-c") != 0)
-            if(strcmp(argv[1], "-s") == 0)
-                if(strcmp(argv[2], "1") == 0)
-                    a = 1;
-                else if(strcmp(argv[2], "2") == 0)
-                    a = 2;
-                else
-                {
-                    printf("Hodnota parametru -s je neplatna\n");
-                    return 100;
-                }
-            else if(strcmp(argv[1], "-l") == 0)
-            {
-                b = atoi(argv[2]);
-                printf("parametr 1 je %d\n", b);
-            }
-            
-            else
-            {
-                printf("zadany parametr neni spravny\n");
-                return 100;
-            }
-        else {
-            printf("zadany parametr neni spravny\n");
-            return 100;
-        }
+    if(se == 1 && (s == 1 || s == 2)){
+        printf("zadany parametr neni spravny\n");
+        return 100;
     }
-        
-    else if(argc == 4)
-    {
-        printf("pocet argumentu je 3");
-        printf("argument 1 je %s\n", argv[1]);
-        if(strcmp(argv[1], "-c") != 0)
-            if(strcmp(argv[1], "-s") == 0)
-                if(strcmp(argv[2], "1") == 0){
-                    a = 1;
-                    printf("parametr 1 je %d\n", a);
-                }
-                else if(strcmp(argv[2], "2") == 0){
-                    a = 2;
-                    printf("parametr 1 je %d\n", a);
-                }
-                else{
-                    printf("Hodnota parametru -s je neplatna\n");
-                    return 100;
-                }
-            else if(strcmp(argv[1], "-l") == 0){
-                b = atoi(argv[2]);
-                printf("parametr 1 je %d\n", b);
-            }
-        if(strcmp(argv[1], "-c") != 0)
-            if(strcmp(argv[1], "-s") == 0)
-                if(strcmp(argv[2], "1") == 0){
-                    a = 1;
-                    printf("parametr 1 je %d\n", a);
-                }
-                else if(strcmp(argv[2], "2") == 0){
-                    a = 2;
-                    printf("parametr 1 je %d\n", a);
-                }
-                else{
-                    printf("Hodnota parametru -s je neplatna\n");
-                    return 100;
-                }
-            else if(strcmp(argv[1], "-l") == 0){
-                b = atoi(argv[2]);
-                printf("parametr 1 je %d\n", b);
-            }
-        printf("argument 2 je %s\n", argv[2]);
-        printf("argument 3 je %s\n", argv[3]);
+    if(le == 1 && l == 0){
+        printf("zadany parametr neni spravny\n");
+        return 100;
     }
 
-    else if(argc == 5){
-        printf("pocet argumentu je 4");
-        printf("argument 1 je %s\n", argv[1]);
-        printf("argument 2 je %s\n", argv[2]);
-        printf("argument 3 je %s\n", argv[3]);
-    }
-
-    else if(argc == 6){
-        printf("pocet argumentu je 5");
-        printf("argument 1 je %s\n", argv[1]);
-        printf("argument 2 je %s\n", argv[2]);
-        printf("argument 3 je %s\n", argv[3]);
-    }
-
-    else
-        printf("pocet argumentu je neplatny");
-    
     return 0;
 }
