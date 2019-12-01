29908e600f32d06dbef684801761fff772762362
IkiHara <riecher.mistt@gmail.com>
2019-12-01 18:47:23 +0100

    hw08v3

--- hw08/main.c
+++ hw08/main.c
@@ -28,16 +28,20 @@ int main(int argc, char *argv[])
             return 100;
         }
         printf("%s\n", argv[i]);
-        printf("c = %i\n", c);
-        printf("s = %i\n", s);
-        printf("l = %i\n", l);
     }
-    if(se == 1 && (s == 1 || s == 2)){
-        printf("zadany parametr neni spravny\n");
+
+    //printf("c = %i\n", c);
+    //printf("se = %i\n", se);
+    //printf("s = %i\n", s);
+    //printf("le = %i\n", le);
+    //printf("l = %i\n", l);
+
+    if(se == 1 && (s == 0 || s > 2)){
+        printf("zadany parametr -s neni spravny\n");
         return 100;
     }
     if(le == 1 && l == 0){
-        printf("zadany parametr neni spravny\n");
+        printf("zadany parametr -l neni spravny\n");
         return 100;
     }
 
